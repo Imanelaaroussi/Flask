7f2d10797561a953f85807ea90aff9013ea83d16
imanelaaroussi <imane.laaroussi.2012@gmail.com>
2024-02-23 18:19:14 -0400

    commitfinalvendredi

--- gestion_taches/app.py
+++ gestion_taches/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template ,request, redirect, url_for,flash,send_file
+from flask import Flask, render_template ,request, redirect, url_for,flash,send_file,jsonify
 import json
 
 app = Flask(__name__)
@@ -22,9 +22,16 @@ def get_all_employees():
         employes = json.load(employes_file)
     return employes
 
+#@app.route("/listeemployes")
+#def liste_employes():
+    # Load tasks from employes.json
+#    with open('employes.json', 'r') as file:
+#        employes = json.load(file)
+    
+    # Render the template with the employes data
+#    return render_template('gestionemployes.html', employes=employes)
 
-  
-from flask import redirect, url_for
+
 
 @app.route("/edit/<titre>", methods=['GET', 'POST'])
 def edit_task(titre):
@@ -77,18 +84,24 @@ def edit_task(titre):
 
 
 
+
+
 @app.route("/delete/<titre>", methods=['POST'])
 def delete_task(titre):
-    # Supprimer la tâche avec le titre donné
-    with open('taches.json', 'r+') as file:
+    # Charger les tâches depuis le fichier taches.json
+    with open('taches.json', 'r') as file:
         taches = json.load(file)
-        taches = [tache for tache in taches if tache['titre'] != titre]
-        file.seek(0)
+    
+    # Filtrer les tâches pour supprimer celle avec le titre donné
+    taches = [tache for tache in taches if tache['titre'] != titre]
+
+    # Réécrire la liste filtrée dans le fichier taches.json
+    with open('taches.json', 'w') as file:
         json.dump(taches, file, indent=4)
-        file.truncate()
     
     # Rediriger vers la page des tâches en cours
     return redirect(url_for('tacheencour'))
+
 
 
 
@@ -161,7 +174,7 @@ def create_task():
     flash('Tâche créée avec succès!', 'success')
 
     # Rediriger vers la page de toutes les tâches
-    return redirect(url_for('touteslestaches'))
+    return render_template('ajouttache.html', employe=employe)
 
 
 
@@ -171,5 +184,150 @@ def get_all_employees():
     return employes
 
 
+
+
+
+# Fonction pour éditer un employé
+@app.route("/edit_employe/<nom>", methods=['GET', 'POST'])
+def edit_employe(nom):
+    if request.method == 'POST':
+        new_nom = request.form['nom']
+        new_prenom = request.form['prenom']
+        new_email = request.form['email']
+        new_icone = request.form['icone']
+
+        # Chargez les employés depuis le fichier JSON
+        with open('employes.json', 'r') as file:
+            employes = json.load(file)
+
+        # Trouvez l'employé à éditer
+        for employe in employes:
+            if employe['nom'] == nom:
+                employe['nom'] = new_nom
+                employe['prenom'] = new_prenom
+                employe['email'] = new_email
+                employe['icone'] = new_icone
+                break
+
+        # Écrivez les données mises à jour dans le fichier JSON
+        with open('employes.json', 'w') as file:
+            json.dump(employes, file, indent=4)
+
+        # Redirigez vers la page de liste des employés
+        return redirect(url_for('liste_employes'))
+    else:
+        # Si la méthode de requête est GET, renvoyez le template d'édition avec les détails de l'employé
+        with open('employes.json', 'r') as file:
+            employes = json.load(file)
+
+        for employe in employes:
+            if employe['nom'] == nom:
+               
+                return render_template('edit_employe.html', employe=employe)
+        
+        # Si l'employé avec l'ID spécifié n'est pas trouvé, redirigez vers la page de liste des employés
+        flash('L\'employé spécifié n\'a pas été trouvé.', 'error')
+        return redirect(url_for('liste_employes'))
+
+
+@app.route("/listeemployes")
+def liste_employes():
+    # Charger les employés depuis le fichier employes.json
+    with open('employes.json', 'r') as file:
+        employes = json.load(file)
+    
+    # Charger les tâches depuis le fichier taches.json
+    with open('taches.json', 'r') as file:
+        taches = json.load(file)
+
+    # Calculer le nombre de tâches en cours pour chaque employé
+    for employe in employes:
+        employe['taches_en_cours'] = sum(1 for tache in taches if isinstance(tache['employe_assigne'], dict) and tache['employe_assigne']['nom'] == employe['nom'] and tache['statut'] == 'en cours')
+
+    # Calculer le nombre total de tâches assignées à chaque employé
+    for employe in employes:
+        employe['taches_total'] = sum(1 for tache in taches if isinstance(tache['employe_assigne'],dict) and tache['employe_assigne']['nom'] == employe['nom'])
+    
+    # Rendre le modèle HTML avec les données des employés
+    return render_template('gestionemployes.html', employes=employes)
+
+
+# Fonction pour supprimer un employé
+@app.route("/delete_employe/<nom>")
+def delete_employe(nom):
+    # Chargez les employés depuis le fichier JSON
+    with open('employes.json', 'r') as file:
+        employes = json.load(file)
+
+    # Trouvez l'employé à supprimer
+    for employe in employes:
+        if employe['nom'] == nom:
+            employes.remove(employe)
+            flash('Cet employé a été supprimé par succés!', 'success')
+            break
+
+    # Écrivez les données mises à jour dans le fichier JSON
+    with open('employes.json', 'w') as file:
+        json.dump(employes, file, indent=4)
+
+    # Redirigez vers la page de liste des employés
+    return render_template('gestionemployes.html', employes=employes)
+
+
+def save_employees(employees):
+    with open('employes.json', 'w') as file:
+        json.dump(employees, file, indent=4)
+
+
+@app.route("/ajoutemploye", methods=['GET', 'POST'])
+def add_employe():
+    if request.method == 'POST':
+        nom = request.form['nom']
+        prenom = request.form['prenom']
+        email = request.form['email']
+        icone = request.form['icone']
+
+        new_employee = {
+            'nom': nom,
+            'prenom': prenom,
+            'email': email,
+            'icone': icone
+        }
+
+        employees = get_all_employees()
+
+        # Vérifier si l'employé existe déjà
+        for employee in employees:
+            if employee['email'] == email:
+                flash('Cet employé existe déjà!', 'error')
+                return render_template('ajoutemploye.html', employes=employes)
+
+        # Si l'employé n'existe pas déjà, l'ajouter à la liste des employés
+        employees.append(new_employee)
+        save_employees(employees)
+
+        flash('Nouvel employé créé avec succès!', 'success')
+        
+        # Mettre à jour la liste des employés avant de rediriger
+        return redirect(url_for('liste_employes'))
+    else:
+        return render_template('ajoutemploye.html', employes=employes)
+
+
+
+@app.route("/export-employees")
+def export_employees():
+    with open('employes.json', 'r') as file:
+        employes = json.load(file)
+    return jsonify(employes)
+
+@app.route("/export-taches-json")
+def export_tasks_json():
+    # Charger les tâches depuis le fichier taches.json
+    with open('taches.json', 'r') as file:
+        tasks = json.load(file)
+    
+    # Envoyer les tâches en tant que fichier JSON
+    return send_file('taches.json', as_attachment=True)
 
 app.run()
